5026b22e633fe59bed5f3f288be4f003cd7bfe11
Nick Melms <nick@craftedagency.com>
2024-08-29 21:43:17 -0400

    basic add function in lesson 2

---
+++ lesson2/app.js
@@ -0,0 +1,7 @@
+var add = function (num1, num2) {
+    return num1 + num2;
+};
+var number1 = "5";
+var number2 = 5;
+var res = add(number1, number2);
+console.log(res);
---
+++ lesson2/app.ts
@@ -0,0 +1,9 @@
+const add = (num1: number, num2: number) => {
+  return num1 + num2;
+};
+
+const number1 = 5;
+const number2 = 5;
+
+const res = add(number1, number2);
+console.log(res);
