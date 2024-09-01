52cfade134699f5da6b251f57c05bfeb1e2fabb1
Nick Melms <nick@craftedagency.com>
2024-09-01 14:43:33 -0400

    tuple

--- lesson2/app.ts
+++ lesson2/app.ts
@@ -1,8 +1,15 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "nick",
   age: 30,
   hobbies: ["sports", "cooking"],
+  role: [2, "author"],
 };
+
 console.log(person.name);
 for (let hobby of person.hobbies) {
   console.log(hobby.toString());
