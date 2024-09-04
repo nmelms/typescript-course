7c8008ae59bc5f26ae28a7e9349cc080782d6f9e
Nick Melms <nick@craftedagency.com>
2024-09-04 14:28:56 -0400

    enums

--- lesson2/app.js
+++ lesson2/app.js
@@ -1,10 +1,20 @@
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "nick",
     age: 30,
-    hobbies: ["sports", "cooking", 1],
+    hobbies: ["sports", "cooking"],
+    role: Role.ADMIN,
 };
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby);
+    console.log(hobby.toString());
+}
+if (person.role === Role.ADMIN) {
+    console.log("is admin");
 }
--- lesson2/app.ts
+++ lesson2/app.ts
@@ -1,16 +1,21 @@
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "nick",
   age: 30,
   hobbies: ["sports", "cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
 console.log(person.name);
 for (let hobby of person.hobbies) {
   console.log(hobby.toString());
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("is admin");
 }
