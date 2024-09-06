765b5bc01d53d95aa013c92661ea32e8a476c6b3
Nick Melms <nick@craftedagency.com>
2024-09-06 18:43:25 -0400

    unknown type

--- lesson2/app.ts
+++ lesson2/app.ts
@@ -1,13 +1,4 @@
-function add(num1: number, num2: number) {
-  return num1 + num2;
-}
+let userInput: unknown;
 
-function printResult(num: number) {
-  console.log("result: ", num);
-}
-
-let combinedValues: (a: number, b: number) => number;
-combinedValues = add;
-// combinedValues = printResult;
-
-console.log(combinedValues(16, 8));
+userInput = "5";
+userInput = 5;
